0ce377f422824b0f8fe13991cdf3d7a3cd996c8c
JannesD <jannesdrijkoningen@gmail.com>
2022-08-30 23:06:18 +0200

    fix and finish obj parser

--- src/objParser.ts
+++ src/objParser.ts
@@ -1,36 +1,46 @@
-const getLinesByType = (lines: string[], type: string) => lines
+const filterByType = (lines: string[]) => (type: string) => lines
   .filter((ln) => ln.startsWith(type))
   .map((ln) => ln.substring(type.length));
 
-const createVec3Extractor = (lines: string[]) => (type: string) => getLinesByType(lines, type)
-  .reduce((acc, ln) => acc.concat(...ln
-    .split(' ')
-    .map((v) => parseFloat(v))), []);
+export interface GLObject {
+  positions: number[],
+  normals: number[],
+  texCoords: number[],
+  vertexCount: number,
+}
 
-export const parse = (objFile: string) => {
-  const lines = objFile
-    .split('\r\n')
-    .filter((ln) => ln !== '' || !ln.startsWith('#'));
+export const parse = (obj: string): GLObject => {
+  const lines = obj
+    .replaceAll('\r', '')
+    .split('\n')
+    .filter((ln) => !ln.startsWith('#') && ln !== '');
 
-  const extractVec3 = createVec3Extractor(lines);
+  const getLinesByType = filterByType(lines);
 
-  const vertices = extractVec3('v  ');
-  const normals = extractVec3('vn ');
-  const textureCoordinates = extractVec3('vt ');
+  const vertices = getLinesByType('v  ').map((ln) => ln.trim().split(' ').map(parseFloat));
+  const texCoords = getLinesByType('vt ').map((ln) => ln.trim().split(' ').map(parseFloat));
+  const normals = getLinesByType('vn ').map((ln) => ln.trim().split(' ').map(parseFloat));
 
-  const indices = getLinesByType(lines, 'f ')
-    .reduce((acc, ln) => {
-      const faceLn = ln.split(' ')
-        .filter((fl) => fl !== '')
-        .map((fl) => parseInt(fl.split('/')[0]) - 1);
+  const objData = [vertices, texCoords, normals];
+  const glData: number[][][] = [[], [], []];
 
-      return acc.concat(...faceLn);
-    }, []);
+  // face is defined as v/vt/vn
+  const faces = getLinesByType('f ');
+  for (const face of faces) {
+    const triangle = face.trim().split(' ').map((t) => t.split('/').map((i) => parseInt(i)));
+    for (const v of triangle) {
+      for (const i of [0, 1, 2]) {
+        glData[i].push(objData[i][v[i] - 1]);
+      }
+    }
+  }
+
+  const flatten = (arr: number[][]) => arr.flatMap((n) => n);
 
   return {
-    vertices,
-    normals,
-    textureCoordinates,
-    indices,
+    positions: flatten(glData[0]),
+    texCoords: flatten(glData[1]),
+    normals: flatten(glData[2]),
+    vertexCount: glData[0].length,
   };
 };
